60e4a1120c98eccaabbffddba87999fa0e70a5f5
Mike Verbeck <mikeverbeck@gmail.com>
2015-01-05 09:27:58 -0500

    Added gtm tag helper

--- app/helpers/rails_gtm/application_helper.rb
+++ app/helpers/rails_gtm/application_helper.rb
@@ -1,4 +1,16 @@
 module RailsGtm
   module ApplicationHelper
+    def gtm_include_tag
+      gtm_container = %Q{<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{Rails.application.config.google_tag_manager_code}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','#{Rails.application.config.google_tag_manager_code}');</script>
+<!-- End Google Tag Manager -->}.html_safe
+      gtm_container
+    end
   end
 end
--- lib/rails_gtm/engine.rb
+++ lib/rails_gtm/engine.rb
@@ -1,5 +1,8 @@
 module RailsGtm
   class Engine < ::Rails::Engine
     isolate_namespace RailsGtm
+    ActiveSupport.on_load :action_controller do
+      helper RailsGtm::Engine.helpers
+    end
   end
 end
